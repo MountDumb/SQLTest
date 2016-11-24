b99d8133d3caf4e4f5d7d8a63a6f357528c38c74
UrbanSkurk <UrbanSkurk@VARMEVRK>
2016-11-24 10:06:21 +0100

    Add project files.

---
+++ SQLTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLTest
+{
+    class Program
+    {
+        private static string connectionString = "Server=ealdb1.eal.local; Database=ejl23_db; User=ejl23_usr; Password=Baz1nga23;";
+        static void Main(string[] args)
+        {
+            Program prog = new SQLTest.Program();
+            prog.Run();
+            Console.ReadLine();
+            
+        }
+
+        private void Run()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("InsertPet", con);
+                    cmd1.CommandType = CommandType.StoredProcedure;
+                    cmd1.Parameters.Add("@Petname", SqlDbType.NVarChar);
+                    cmd1.Parameters["@PetName"].Value = "Perdy";
+                    cmd1.Parameters.Add(new SqlParameter("@PetType", "Cat"));
+                    cmd1.Parameters.Add(new SqlParameter("@PetWeight", 2.54m));
+
+                    cmd1.ExecuteNonQuery();
+                    //con.Open();
+                    //SqlCommand cmd1 = new SqlCommand("InsertPet", con);
+                    //cmd1.CommandType = CommandType.StoredProcedure;
+                    //cmd1.Parameters.Add("@Petname", SqlDbType.NVarChar);
+                    //cmd1.Parameters["@PetName"].Value = "Kalle";
+                    //cmd1.Parameters.Add(new SqlParameter("@PetType", "Kalle"));
+                    //cmd1.Parameters.Add(new SqlParameter("@PetBreed", "Kalle"));
+                    //cmd1.Parameters.Add(new SqlParameter("@PetDOB", DateTime.Now));
+                    //cmd1.Parameters.Add(new SqlParameter("@PetWeight", 2.54m));
+
+                    //cmd1.ExecuteNonQuery();
+
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Ooops, you got the following errormessage: " + e.Message);
+                }
+            }
+        }
+    }
+}
